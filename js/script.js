fbd5bd202355ec2648539c86036fea2af8070099
Prico3 <pricoco.andrea@gmail.com>
2022-10-10 16:33:04 +0200

    push per correzione

--- js/script.js
+++ js/script.js
@@ -2,13 +2,23 @@
 // ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
 // Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
+
+const row = document.querySelector(".row");
+
 for (var i=1; i <= 100; i++) {
-    if (i % 15 == 0)
-        console.log("FizzBuzz");
-    else if (i % 3 == 0)
-        console.log("Fizz");
-    else if (i % 5 == 0)
-        console.log("Buzz");
-    else
-        console.log(i);
+
+    let result = "";
+
+    if (i % 15 == 0) {
+        result = "FizzBuzz";
+    } else if (i % 3 == 0) {
+        result = "Fizz"; 
+    } else if (i % 5 == 0) {
+        result = "Buzz";
+    }
+
+    console.log(result);
+
 }
+const cell = `<div class="cell ${result}">${i}</div>`;
+row.innerHTML += cell;
